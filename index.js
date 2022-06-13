c3281ad66929dc3bf18cf029e271a7fcc5834274
R a b i a <rabia.saeed.1995@gmail.com>
2022-06-13 18:37:43 -0400

    creating the classes Employee and Manager

---
+++ index.js
@@ -0,0 +1,15 @@
+const inquirer = require("inquirer");
+const jest = require("jest");
+const path = require("path");
+
+// array of questions for generating a team profile
+const questions = [
+	{
+		type: "input",
+		name: "name",
+		message: "What is your name?",
+	},
+	{ type: "input", name: "email", message: "What is your email?" },
+
+	{ type: "input", name: "github", message: "What is your GitHub username?" },
+];
